adfe3e6d1714f6e5ded7e2b322b41aaf472ca872
Víctor Mejía <vmejiaec@gmail.com>
2023-02-18 20:41:32 -0500

    Anado atributo Name, cambio constructor, add constructor con param

--- ventana.cs
+++ ventana.cs
@@ -4,8 +4,15 @@ namespace miweb;
 // Clase ventana
 public class Ventana{
   public int Id ;
+  public string Name;
 
   public Ventana(){
     Id = 1;
+    Name = "Ventana vacía";
+  }
+
+  public Ventana(int Id , string Name){
+    this.Id = Id;
+    this.Name = Name;
   }
 }
