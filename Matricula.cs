d771375d72cbd02cbb99bb48ba0ed4793b8465ec
victormejia3 <victor.mejia@itq.edu.ec>
2023-02-19 22:11:19 -0500

    Se añaden dos constructores a Matricula

--- Matricula.cs
+++ Matricula.cs
@@ -2,4 +2,11 @@ namespace itqweb;
 
 public class Matricula{
     public string estudiante;
+    public Matricula(){
+        estudiante = "Ninguno";
+    }
+
+    public Matricula(string estudiante){
+        this.estudiante = estudiante;
+    }
 }
