02bdd0cc8295179194930549f3292880e718c08d
Víctor Mejía <vmejiaec@gmail.com>
2023-02-19 19:54:31 -0500

    Añado tipo de menú de Financiero

--- Menu.cs
+++ Menu.cs
@@ -3,6 +3,9 @@ namespace miweb;
 public class Menu{
     public string tipo;
     public setTipoInv(){}
-    public setTipoFin(){}
+    public setTipoFin(){
+        // Pongo el tipo del menú a Financiero
+        this.tipo = "Financiero";
+    }
     public setTipoMat(){}
 }
